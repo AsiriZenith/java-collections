0fef652a40055bf546df0d49a0c9d0966488d76e
Asiri Senith <asirisenith@gmail.com>
2020-08-23 16:12:36 +0530

    Implemented program using ArrayList

---
+++ ArrayList1.java
@@ -0,0 +1,27 @@
+import java.util.*; 
+import java.util.Collections;
+
+class ArrayList1{
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Asiri");
+        list.add("Senith");
+        list.add("Manjitha");
+
+        //Traversing list through Iterator
+        // Iterator itr = list.iterator();
+      
+        // while (itr.hasNext()) {
+        //     System.out.print(itr.next());
+        //     System.out.print(" ");
+        // }
+        System.out.println(list);
+        list.add(2,"AAA");
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
