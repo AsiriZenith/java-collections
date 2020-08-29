72e0705975b48b639c2e1e83ccb6c34f214641d3
Asiri Senith <asirisenith@gmail.com>
2020-08-29 17:44:57 +0530

    Created Linked Hash set in java

--- LinkedHashSet1.java
+++ LinkedHashSet1.java
@@ -1,12 +1,38 @@
-import java.util.*;  
+import java.util.*;
 
-class HashSet3 {
+class Book {
 
-    int id;  
-    String name,author,publisher;  
-    int quantity;
-    
-    
+    private int id;
+    private String name, author, publisher;
+    private int quantity;
+
+    public Book(int id, String name, String author, String publisher, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
 
 /**
@@ -14,5 +40,19 @@ class HashSet3 {
  */
 public class LinkedHashSet1 {
 
-    
+    public static void main(String[] args) {
+        LinkedHashSet<Book> hs = new LinkedHashSet<Book>();
+
+        Book b1 = new Book(101, "Let us C", "Yashwant Kanetkar", "BPB", 8);
+        Book b2 = new Book(102, "Data Communications & Networking", "Forouzan", "Mc Graw Hill", 4);
+        Book b3 = new Book(103, "Operating System", "Galvin", "Wiley", 6);
+
+        hs.add(b1);
+        hs.add(b2);
+        hs.add(b3);
+
+        for (Book b : hs) {
+            System.out.println(b.getId() + " " + b.getName() + " " + b.getAuthor() + " " + b.getPublisher() + " " + b.getQuantity());
+        }
+    }
 }
